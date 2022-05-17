8bd25057268d6847cb30b13c4c1e287357b4752c
azzy kesuma <official.azzy@gmail.com>
2022-05-17 16:26:07 +0700

    adding signup and sign in features

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { 
   Container,
   Box,
@@ -8,13 +9,53 @@ import {
   FormGroup,
   FormControlLabel,
   Checkbox,
+  Alert
  } from '@mui/material'
 
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GoogleIcon from '@mui/icons-material/Google';
+import Link from 'next/link';
+// firebase functions
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 
 export default function Home() {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [checked,setChecked] = useState(false);
+  const [error,setError] = useState("");
+  const [success,setSuccess] = useState("");
+
+  const handleEmailChange = (e) => {
+      setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+      setPassword(e.target.value);
+  }
+
+  const handleChecked = e => {
+    setChecked(!checked);
+  }
+
+  const handleSubmit = e => {
+      e.preventDefault()
+      const auth = getAuth();
+
+    if(checked){
+      createUserWithEmailAndPassword(auth,email,password)
+      .then(cred => {
+          console.log(cred.user);
+          setError('');
+          setSuccess('User created successfully, redirecting to homepage in 3 seconds...');
+      })
+      .catch(err => {
+          console.log(err.message);
+      })
+    } else {
+        setError('Please accept the terms and conditions');
+    }
+  }
   return (
     <Container
     maxWidth="md"
@@ -46,6 +87,14 @@ export default function Home() {
       >
         Create an Account
       </Typography>
+      <Box
+      sx={{
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"center",
+        alignItems:"center",
+      }}
+      >
         <Button
         variant='contained'
         color='primary'
@@ -66,6 +115,7 @@ export default function Home() {
           marginBottom: '20px'
         }}
         >Sign up with Facebook</Button>
+      </Box>
         <Divider />
         <Typography
         variant='body3'
@@ -80,11 +130,18 @@ export default function Home() {
           {/* sign up using email */}
           <Box
           component='form'
+          onSubmit={handleSubmit}
           >
+            {error && <Alert severity="error" sx={{marginBottom : '20px'}}>{error}</Alert>}
+            {success && <Alert severity="success" sx={{marginBottom : '20px'}}>{success}</Alert>}
             <TextField
             id='email'
             variant='outlined'
             label='Email'
+            type='email'
+            value={email}
+            onChange={handleEmailChange}
+            required
             placeholder='E.g : JohnDoe@gmail.com'
             sx={{
               width: '100%',
@@ -95,6 +152,10 @@ export default function Home() {
             id='password'
             variant='outlined'
             label='Password'
+            type='password'
+            value={password}
+            onChange={handlePasswordChange}
+            required
             placeholder='must at least 6 character strong'
             sx={{
               width: '100%',
@@ -103,7 +164,7 @@ export default function Home() {
             />
             <FormGroup>
               <FormControlLabel 
-              control={<Checkbox />}
+              control={<Checkbox checked={checked} onChange={handleChecked} />}
               label="I agree with the Term of Service"/>
             </FormGroup>
             <Button
@@ -117,6 +178,13 @@ export default function Home() {
             >
               Create my Account
             </Button>
+            <Typography
+            component='p'
+            variant='body3'
+            sx={{
+              mt: '20px'
+            }}
+            >Already have an Account? <Link href="/signin">Sign In</Link></Typography>
           </Box>
       </Box>
       
---
+++ pages/signin.js
@@ -0,0 +1,123 @@
+import { useState } from 'react';
+import { 
+    Box, 
+    Container,
+    Typography,
+    TextField,
+    Button
+} from "@mui/material";
+import Link from 'next/link'
+// firebase functions
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const Signin = () => {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [error,setError] = useState("");
+  
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+  
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth,email,password)
+            .then(cred => {
+                console.log(`user ${cred.user.email} signed in`);
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
+    }
+    return (
+        <Container
+        maxWidth='md'
+        sx={{
+            display:"flex",
+            flexDirection:"column",
+            justifyContent:"center",
+            alignItems:"center",
+            height: "100vh",
+          }}
+        >
+            <Box
+            sx={{
+                border: '1px solid grey',
+                padding : '20px',
+                borderRadius : '5px',
+                width: '80%',
+                textAlign: 'center',
+              }}
+            > 
+            <Typography
+            variant='h5'
+            component='h1'
+            sx={{
+                fontFamily: 'Quicksand',
+                fontWeight : '500',
+                mb: '20px'
+            }}>
+                Sign in
+            </Typography>
+            <Box
+            component='form'
+            onSubmit={handleSubmit}
+            >
+            <TextField
+            id='email'
+            variant='outlined'
+            label='Email'
+            placeholder='E.g : JohnDoe@gmail.com'
+            value={email}
+            onChange={handleEmailChange}
+            type='email'
+            required
+            sx={{
+              width: '100%',
+              marginBottom : '20px'
+            }}
+            />
+            <TextField
+            id='password'
+            variant='outlined'
+            label='Password'
+            value={password}
+            onChange={handlePasswordChange}
+            type='password'
+            required
+            placeholder='must at least 6 character strong'
+            sx={{
+              width: '100%',
+              marginBottom : '20px'
+            }}
+            />
+            <Button
+            variant='contained'
+            color="primary"
+            type='submit'
+            sx={{
+              marginTop: '20px',
+              width : '250px',
+            }}
+            >
+              Sign in
+            </Button>
+            <Typography
+            component='p'
+            variant='body3'
+            sx={{
+              mt: '20px'
+            }}
+            >Don't have an Account? <Link href="/">Sign Up</Link> here</Typography>
+            </Box>
+        </Box>
+        </Container>
+    );
+}
+ 
+export default Signin;
